a2c0720f29b65d8f4956ded461e9be10b2f4410f
erlie <lilimin@social-touch.com>
2018-03-13 10:25:50 +0800

    测试git

--- src/main/java/com/makenv/MallApplication.java
+++ src/main/java/com/makenv/MallApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.makenv.dao")
+//测试一下
 public class MallApplication {
 
 	public static void main(String[] args) {

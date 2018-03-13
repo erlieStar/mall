fb3f306b5cb39e6d1ca4eff6d6c768f923e549cc
erlie <lilimin@social-touch.com>
2018-03-13 10:53:33 +0800

    测试ssh

--- src/main/java/com/makenv/MallApplication.java
+++ src/main/java/com/makenv/MallApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.makenv.dao")
-//测试一下
 public class MallApplication {
 
 	public static void main(String[] args) {
